dc886e548de90f0ad12bcc31c4f535914f3adb5a
NAJMI <mnajmi2912@gmail.com>
2024-04-28 18:25:41 +0800

    change format openai

--- Chatbot_app.py
+++ Chatbot_app.py
@@ -23,7 +23,7 @@ if prompt := st.chat_input("What is up?"):
   with st.chat_message("assistant"):
     message_placeholder = st.empty()
     full_response = ""
-    for response in openai.ChatCompletion.create(
+    for response in openai.chat.completions.create(
       model=st.session_state["openai_model"],
       messages=[
         {"role": m["role"], "content": m["content"]}
